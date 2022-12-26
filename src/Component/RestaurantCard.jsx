cccd1275437ee4fd0e68f9f96e0a7ce161e72cc6
RAVINDER NANDAL <ravindernandal366@gmail.com>
2022-12-26 08:34:27 +0000

    Update 4 files

--- src/Component/RestaurantCard.jsx
+++ src/Component/RestaurantCard.jsx
@@ -1,8 +1,23 @@
 import styles from "./RestaurantCard.module.css"
-function RestaurantCard({ name, image,type,rating,price_starts_from,number_of_votes }) {
+function RestaurantCard(props) {
+  var obj = props.obj;
+  
+  var name = obj.name;
+  var type = obj.type;
+  var rating = obj.rating;
+  var price = obj.price_starts_from;
+  var votes = obj.number_of_votes;
+  var image = obj.image;
+  
   return (
   <div data-testid="restaurant-card" className={styles.container}>
    {/* display the props */}
+   <img src={image}/>
+   <h3>{name}</h3>
+   <p>{type}</p>
+   <p> {rating} </p>
+   <p> {price} </p>
+   <p> Number of votes : {votes} </p>
     </div>
   );
 }
